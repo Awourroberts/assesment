245160b2a72f0cbe10eff0243fcbfb57e21b5ad8
Awourrobberrt <sophierobberrt@gmail.com>
2021-05-06 12:18:44 +0300

    first commit

---
+++ src/lynnch.kt
@@ -0,0 +1,23 @@
+fun main() {
+    println("sophy" ,20)
+    println("drinks")
+
+}
+fun introduction(var name:String, var age:Int){
+    var word("my name is $ name and  I am  $ age  year old")
+    return(word)
+
+}fun students(){
+    for(age <=6){
+        println("milk")
+
+    }
+    if (age=>15){
+        println("fant")
+    }
+    else(age=>6){
+        println("cocola")
+    }
+}
+
+
